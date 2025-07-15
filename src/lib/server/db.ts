d8c50e79a101f4823fd3a8d338a22f9fdcf0a312
crevalen <aldimaulana251298@gmail.com>
2025-07-16 06:33:25 +0700

    PRISMA ACCEL

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,5 +1,6 @@
 // src/lib/server/db.ts
-
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-export const db = new PrismaClient();
+// Inisialisasi client dan langsung tambahkan ekstensi Accelerate
+export const db = new PrismaClient().$extends(withAccelerate());
